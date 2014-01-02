4d9bfc97feeb825e2c1b88648575f41ca559a543
sjau <github.com@sjau.ch>
2014-01-02 09:58:30 +0100

    Added ISPConfig / Horde installer; problem with mysql root preseed

--- install.sh
+++ install.sh
@@ -45,6 +45,21 @@ mailmanpassword="mypassword"
 jailkit="y"     # Set to 'y' to enable Jailkit
 
 
+### ISPConfig
+databasename="dbispconfig"        # Set ISPConfig Database Name
+port="8080"     # Set ISPConfig Port
+ssl="y" # Set to 'y' to use SSL to access ISPCConfig
+
+
+### Horde
+horde="y"       # Set 'y' to install Horde
+hordedatabase="horde"   # Set Horde Databasename
+hordeuser="horde"       # Set MySQL Horde Username
+hordepassword="mypassword"      # Set password for Horde Username
+hordefilesystem="/var/www/horde"        # Set filesystem location for horde
+hordeadmin="admin@mydomain"     # Set existing mail user with administrator permissions
+
+
 # Detailed installation steps at http://www.howtoforge.com/perfect-server-debian-wheezy-apache2-bind-dovecot-ispconfig-3
 
 
@@ -62,7 +77,6 @@ function updateSettings
         File="$1"
         Pattern="$2"
         Replace="$3"
-
         if grep "${Pattern}" "${File}" >/dev/null;
         then
                 # Pattern found, replace line
@@ -204,16 +218,12 @@ deb-src http://security.debian.org/ wheezy/updates main contrib non-free
 # wheezy-updates, previously known as 'volatile'
 deb http://ftp.${country,,}.debian.org/debian/ wheezy-updates main contrib non-free
 deb-src http://ftp.${country,,}.debian.org/debian/ wheezy-updates main contrib non-free" > "/etc/apt/sources.list"
-
     apt-get update
-
     apt-get -y install debconf-utils
     echo -e "debconf debconf/frontend select Noninteractive\ndebconf debconf/priority select critical" | debconf-set-selections
     preseeding
     cat packages.preseed | debconf-set-selections
     apt-get -y install openssh-server ntp ntpdate postfix postfix-mysql postfix-doc mysql-client mysql-server openssl getmail4 rkhunter binutils dovecot-imapd dovecot-pop3d dovecot-mysql dovecot-sieve sudo amavisd-new spamassassin clamav clamav-daemon zoo unzip bzip2 arj nomarch lzop cabextract apt-listchanges libnet-ldap-perl libauthen-sasl-perl clamav-docs daemon libio-string-perl libio-socket-ssl-perl libnet-ident-perl zip libnet-dns-perl apache2 apache2.2-common apache2-doc apache2-mpm-prefork apache2-utils libexpat1 ssl-cert libapache2-mod-php5 php5 php5-common php5-gd php5-mysql php5-imap phpmyadmin php5-cli php5-cgi libapache2-mod-fcgid apache2-suexec php-pear php-auth php5-mcrypt mcrypt php5-imagick imagemagick libapache2-mod-suphp libruby libapache2-mod-ruby libapache2-mod-python php5-curl php5-intl php5-memcache php5-memcached php5-ming php5-ps php5-pspell php5-recode php5-snmp php5-sqlite php5-tidy php5-xmlrpc php5-xsl memcached php5-xcache libapache2-mod-fastcgi php5-fpm expect pure-ftpd-common pure-ftpd-mysql quota quotatool bind9 dnsutils vlogger webalizer awstats geoip-database libclass-dbi-mysql-perl fail2ban
-
-    dpkg-reconfigure dash
     update-rc.d -f spamassassin remove
 
 }
@@ -278,16 +288,7 @@ function configureMailman
     case "${mailman}" in
         y)  echo "Installing Mailman"
             apt-get -y install mailman
-            mailmanexpect=$(expect -c "
-spawn newlist mailman
-expect \"list:\"
-send \"${mailmanemail}\r\"
-expect \"password:\"
-send \"${mailmanpassword}\r\"
-expect \"...\"
-send \"\r\"
-")
-            echo "${mailmanexpect}"
+            ./mailmanexpect "${mailmanemail}" "${mailmanpassword}"
             echo '
 ## mailman mailing list
 mailman:              "|/var/lib/mailman/mail/mailman post mailman"
@@ -393,6 +394,102 @@ ignoreregex =' > "/etc/fail2ban/filter.d/dovecot-pop3imap.conf"
 }
 
 
+
+function installISPConfig
+{
+    if [[ -z "${commonname}" ]]
+    then
+        commonname="${hostname}.${domain}"
+    fi
+    wget "http://www.ispconfig.org/downloads/ISPConfig-3-stable.tar.gz" -O "/tmp/ISPC.tgz"
+    tar -xvzf "/tmp/ISPC.tgz" -C "/tmp/"
+    ./ispcexpect "${mysqlpassword}" "${databasename}" "${country}" "${state}" "${city}" "${organization}" "${unit}" "${commonname}" "${email}" "${port}" "${ssl}" "${country}" "${state}" "${city}" "${organization}" "${unit}" "${email}"
+}
+
+
+
+function installHorde
+{
+    case "${horde}" in
+        y)  echo "Installing Horde"
+            apt-get -y install php5-sasl php5-intl libssh2-php php5-curl php-http php5-xmlrpc php5-geoip php5-ldap php5-memcache php5-memcached php5-tidy
+            pear channel-discover pear.horde.org
+            pear install horde/horde_role
+            pear run-scripts horde/horde_role
+            pear install -a -B horde/webmail
+            ./mysqlexpect "${mysqlpassword}" "${hordedatabase}" "${hordeuser}" "${hordepassword}"
+            ./hordeexpect "${hordeuser}" "${hordepassword}" "${hordedatabase}" "${hordefilesystem}" "${hordeadmin}"
+            mkdir "${hordefilesystem}/phptmp/"
+            chown -R www-data:www-data "${hordefilesystem}"
+            pear install channel://pear.php.net/SOAP-0.13.0
+            pear install pear/MDB2#mysql
+            pear install channel://pear.php.net/HTTP_WebDAV_Server-1.0.0RC7
+            pear install channel://pear.php.net/XML_Serializer-0.20.2
+            pear install channel://pear.php.net/Date_Holidays-0.21.6
+            pear install Net_LDAP
+            pear install channel://pear.php.net/Text_CAPTCHA-0.4.3
+            pear install pear/HTTP_Request2
+            pear install channel://pear.php.net/Console_Color2-0.1.1
+            echo "Alias /Microsoft-Server-ActiveSync ${hordefilesystem}/rpc.php
+Alias /horde ${hordefilesystem}
+<Directory ${hordefilesystem}>
+           Options +FollowSymLinks
+           AllowOverride All
+           order allow,deny
+           allow from all
+           AddType application/x-httpd-php .php
+           php_value include_path \".:/usr/share/php\"
+           php_value open_basedir \"none\"
+           php_value upload_tmp_dir \"${hordefilesystem}/phptmp/\"
+</Directory>" > /etc/apache2/conf.d/horde.conf
+            updateSettings "/var/www/horde/.htaccess" 'RewriteEngine On' "    RewriteEngine On\n    RewriteBase /horde"
+            pear install -a -B horde/passwd
+            chown -R www-data:www-data "${hordefilesystem}/passwd"
+            cp -a "${hordefilesystem}/passwd/config/backends.php" "${hordefilesystem}/passwd/config/backends.local.php"
+            echo "\$backends['sql'] = array (
+  'disabled' => false,
+  'name' => 'SQL Server',
+  'preferred' => '',
+  'policy' => array(
+    'minLength' => 7,
+    'maxLength' => 64,
+    'maxSpace' => 0,
+    'minNumeric' => 1,
+  ),
+  'driver' => 'Sql',
+  'params' => array(
+    'phptype' => 'mysql',
+    'hostspec' => 'localhost',
+    'username' => 'root',
+    'password' => '${mysqlpassword}',
+    'encryption' => 'crypt-md5',
+    'database' => '${databasename}',
+    'table' => 'mail_user',
+    'user_col' => 'email',
+    'pass_col' => 'password',
+    'show_encryption' => false
+    // The following two settings allow you to specify custom queries for
+    // lookup and modify functions if special functions need to be
+    // performed. In places where a username or a password needs to be
+    // used, refer to this placeholder reference:
+    // %d -> gets substituted with the domain
+    // %u -> gets substituted with the user
+    // %U -> gets substituted with the user without a domain part
+    // %p -> gets substituted with the plaintext password
+    // %e -> gets substituted with the encrypted password
+    //
+    // 'query_lookup' => 'SELECT user_pass FROM horde_users WHERE user_uid = %u',
+   // 'query_modify' => 'UPDATE horde_users SET user_pass = %e WHERE user_uid = %u',
+  ),
+);" > "${hordefilesystem}/passwd/config/backends.local.php"
+            ;;
+        *)  echo ""
+    esac
+}
+
+
+
+dpkg-reconfigure dash
 configureNetwork
 installPackages
 configureMySQL
@@ -404,9 +501,12 @@ configureQuota
 configureAWstats
 configureJailkit
 configureFail2ban
+installISPConfig
+installHorde
 
 
-
+#clear;
+echo "Installation of Perfect Server, ISPConfig and - if selected - Horde Webmail complete. Please reboot server and connect to new IP if it was altered."
 
 
 #### At the end make a reboot - to also use new network settings
